b3af8873c0872c3d93a892337b202e34db6c54bd
Fabian Mueller <mueller.fabian@gmail.com>
2015-09-27 11:54:27 +0200

    Cleanup stuff from starter kit

--- test/app/app.e2e.js
+++ test/app/app.e2e.js
@@ -15,24 +15,4 @@ describe('App', function() {
     expect(subject).toEqual(result);
   });
 
-  it('should have a title', function() {
-    subject = browser.getTitle();
-    result  = 'Angular2 Webpack Starter by @gdi2990 from @AngularClass';
-  });
-
-  it('should have <header>', function() {
-    subject = element(by.deepCss('app /deep/ header')).isPresent();
-    result  = true;
-  });
-
-  it('should have <main>', function() {
-    subject = element(by.deepCss('app /deep/ main')).isPresent();
-    result  = true;
-  });
-
-  it('should have <footer>', function() {
-    subject = element(by.deepCss('app /deep/ footer')).getText();
-    result  = 'WebPack Angular 2 Starter by @AngularClass';
-  });
-
 });
