f5c7ec9d249f00e70267be0f0ee5275a5df86c00
Fabian Mueller <mueller.fabian@gmail.com>
2015-09-30 08:12:56 +0200

    Run tests continously

--- karma.conf.js
+++ karma.conf.js
@@ -84,7 +84,7 @@ module.exports = function(config) {
 
 
     // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
+    autoWatch: true,
 
 
     // start these browsers
@@ -94,7 +94,7 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true
+    singleRun: false
   };
   config.set(_config);
 };
